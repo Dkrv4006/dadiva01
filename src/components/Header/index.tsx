e7438880a0fe387bdacb8858b28c7f3beed1c076
Dkrv4006 <danielkennedy400619@gmail.com>
2023-03-14 22:19:50 -0300

    v 0.0.1

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+
+import { Container } from "./style"
+
+export const Header: React.FC = () => {
+  let da = 12
+  return (
+    <Container>
+      <div className="logo">
+      <h1><em>DADIVA</em></h1>
+      <h3><em>LOGISTICA</em></h3>
+"xdc"
+      </div>
+    </Container>
+  )
+  
+}
---
+++ src/components/Header/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100px;
+    
+    padding: 20px;
+    background-color: var(--primary);
+h3{
+    margin-left: 40px;
+    
+}
+
+    
+
+`
