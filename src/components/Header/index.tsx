dcb5ab64408d47b35ef2ab0c9bfdd87e98aecf24
Dkrv4006 <danielkennedy400619@gmail.com>
2023-03-17 16:28:33 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,38 @@
 
 import { Container } from "./style"
+import { Grid } from 'gridjs-react';
 
+import "gridjs/dist/theme/mermaid.css";
 export const Header: React.FC = () => {
   let da = 12
   return (
     <Container>
       <div className="logo">
-      <h1><em>DADIVA</em></h1>
-      <h3><em>LOGISTICA</em></h3>
-"xdc"
       </div>
+
+      <Grid
+  data={[
+    ['John', 'john@example.com'],
+  
+    ['Mike', 'mike@gmail.com']
+  ]
+}
+  resizable={true}
+  wi
+  columns={['Name', 'Email','Phone','Cep','Name', 'Email','Phone','Cep']}
+  search={true}
+  pagination={{
+    limit: 2,
+  }}
+  style={{
+    td: {
+      border: '1px solid #f00e0e'
+    },
+    table: {
+      'font-size': '15px'
+    }
+  }}
+/>
     </Container>
   )
   
